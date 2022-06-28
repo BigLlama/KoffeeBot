00c59c27011bc83813cf282026080d72655c2e5e
BigLlama <97636888+BigLlama@users.noreply.github.com>
2022-06-28 11:34:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -64,16 +64,9 @@ async def on_ready():
     print('Online as {0.user}'.format(client))
     print(f"Currently in {len(client.guilds)} servers")
 
-
-   # discord_guild = client.guilds[-1]
-   # print(discord_guild)
-   # invite = await discord_guild.text_channels[0].create_invite(max_uses=1)
-   # print(invite)
-
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
-
 
 @client.event  # on guild join
 async def on_guild_join(guild):
@@ -151,107 +144,6 @@ async def servers(ctx, arg=None):
     embed.add_field(name=f"Currently in {len(client.guilds)} servers", value="Add me to more :D\n"
                          "https://top.gg/bot/901223515242508309?s=0210af7e1c4e5")
     await ctx.send(embed=embed)
-
-
-@client.command()  # tic tac toe
-async def tictactoe(ctx, p1: discord.Member, p2: discord.Member):
-
-    global count
-    global player1
-    global player2
-    global turn
-    global gameOver
-
-    if gameOver:
-        global board
-        board = [":white_large_square:", ":white_large_square:", ":white_large_square:", ":white_large_square:",
-                 ":white_large_square:", ":white_large_square:",
-                 ":white_large_square:", ":white_large_square:", ":white_large_square:"]
-        turn = ""
-        gameOver = False
-        count = 0
-
-        player1 = p1
-        player2 = p2
-
-        # print the board
-        line = ""
-        for x in range(len(board)):
-            if x == 2 or x == 5 or x == 8:
-                line += " " + board[x]
-                await ctx.send(line)
-                line = ""
-            else:
-                line += " " + board[x]
-
-        # determine who goes first
-        num = random.randint(1, 2)
-        if num == 1:
-            turn = player1
-            await ctx.send("It is <@" + str(player1.id) + ">'s turn.")
-        elif num == 2:
-            turn = player2
-            await ctx.send("It is <@" + str(player2.id) + ">'s turn.")
-    else:
-        await ctx.send("A game is already in progress! Finish it before starting a new one.")
-
-
-@client.command()
-async def place(ctx, pos: int):
-    global turn
-    global player1
-    global player2
-    global board
-    global count
-    global gameOver
-
-    if not gameOver:
-        mark = ""
-        if turn == ctx.author:
-            if turn == player1:
-                mark = ":regional_indicator_x:"
-            elif turn == player2:
-                mark = ":o2:"
-            if 0 < pos < 10 and board[pos - 1] == ":white_large_square:":
-                board[pos - 1] = mark
-                count += 1
-
-                # print the board
-                line = ""
-                for x in range(len(board)):
-                    if x == 2 or x == 5 or x == 8:
-                        line += " " + board[x]
-                        await ctx.send(line)
-                        line = ""
-                    else:
-                        line += " " + board[x]
-
-                checkwinner(winningconditions, mark)
-                print(count)
-                if gameOver is True:
-                    await ctx.send(mark + " wins!")
-                elif count >= 9:
-                    gameOver = True
-                    await ctx.send("It's a tie!")
-
-                # switch turns
-                if turn == player1:
-                    turn = player2
-                elif turn == player2:
-                    turn = player1
-            else:
-                await ctx.send("Be sure to choose an integer between 1 and 9 (inclusive) and an unmarked tile.")
-        else:
-            await ctx.send("It is not your turn.")
-    else:
-        await ctx.send("Please start a new game using the !tictactoe command.")
-
-
-def checkwinner(winning_conditions, mark):
-    global gameOver
-    for condition in winning_conditions:
-        if board[condition[0]] == mark and board[condition[1]] == mark and board[condition[2]] == mark:
-            gameOver = True
 
 load_dotenv()
 token = environ["TOKEN"]
